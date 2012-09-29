26e8e734a6847a9baf061a504f212743f8149d44
German Irias <gei08a@acu.edu>
2012-09-29 17:08:32 -0500

    Initial commit

---
+++ coltrane/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import get_object_or_404, render_to_response
+from coltrane.models import Entry
+
+def entries_index(request):
+    return render_to_response('coltrane/entry_index.html',
+                              { 'entry_list': Entry.objects.all() })

54e23845a8b093999180be80f20cb6cfcf87e196
German Irias <gei08a@acu.edu>
2012-10-13 14:13:12 -0500

    included generic views

--- coltrane/views.py
+++ coltrane/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render_to_response
-from coltrane.models import Entry
+from django.views.generic.list_detail import object_list
+from coltrane.models import Category
 
-def entries_index(request):
-    return render_to_response('coltrane/entry_index.html',
-                              { 'entry_list': Entry.objects.all() })
+def category_detail(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    return object_list(request, queryset=category.entry_set.all(),
+                       extra_context={ 'category': category.live_entry_set() })
